39495981191038075b83a67fa9aa6994e809854f
Piyanwalker <145029477+Piyanwalker@users.noreply.github.com>
2024-11-01 02:31:30 +0700

    Add files via upload

---
+++ pertemuan5/latihan/SQLQuery_5.sql
@@ -0,0 +1,11 @@
+INSERT INTO dosen_wali (nip, nama)
+VALUES ('2330171', 'Sopyan Nur Avin'),
+('465446', 'ripan'),
+('432432', 'putra'),
+('5234534' 'denis'),
+('8423894', 'joo'),
+('8423543', 'rudol'),
+('8441213', 'vani'),
+('6324776', 'huto'),
+('7543599', 'susi'),
+('2423356', 'rustam');
